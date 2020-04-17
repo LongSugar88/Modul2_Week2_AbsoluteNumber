5e0c4859b465d8451ed8f742bcc7d28727ee7cb3
longduongxd <longduongxd@gmail.com>
2020-04-17 23:23:15 +0700

    Tính giá trị tuyệt đối của một số

---
+++ Test/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+
+    @Test
+    void testFindAbsolute0() {
+        int number = 0;
+        int expect = 0;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expect, result);
+    }
+    @Test
+    void testFindAbsolute1(){
+        int number = 1;
+        int expect = 1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expect, result);
+    }
+    @Test
+    void testFindAbsoluteNegative1(){
+        int number = -1;
+        int expect = 1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expect, result);
+    }
+
+
+
+}
